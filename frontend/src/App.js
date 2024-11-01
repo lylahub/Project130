3d0a69ea81ad5d5217885a25718c61341ccb6d75
baby-ww <howardwang.u@gmail.com>
2024-10-31 17:09:55 -0700

    fix main page route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,35 @@
+// src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, useLocation, Navigate } from 'react-router-dom';
 import { AnimatePresence } from "framer-motion";
 import { UserProvider } from "./userContext.js";
 import "./App.css";
 import './css/theme.css';
-
 
 // import pages
 import Home from './pages/Home.js';
 import GroupSplit from './pages/GroupSplit.js';
 import IncomeRecommendation from './pages/IncomeRe.js';
 
-
 function AppRouter() {
   const location = useLocation();
   return (
     // <UserProvider>
     // for later user login
-      <AnimatePresence mode="wait">
-        <Routes location={location} key={location.pathname}>
-          {/* <Route exact path="/" element={<LoginSignup />} /> */}
-          <Route exact path="/sb" element={<Home />} />
-          <Route path="/group-split" element={<GroupSplit />} />
-          <Route path="/income-re" element={<IncomeRecommendation />} />
-          {/* new pages put here */}
-        </Routes>
-      </AnimatePresence>
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        {/* Redirect root to /sb */}
+        <Route path="/" element={<Navigate to="/sb" replace />} />
+        
+        {/* Main routes */}
+        <Route path="/sb" element={<Home />} />
+        <Route path="/sb/group-split" element={<GroupSplit />} />
+        <Route path="/sb/income-re" element={<IncomeRecommendation />} />
+        
+        {/* Redirect unmatched routes to /sb */}
+        <Route path="*" element={<Navigate to="/sb" replace />} />
+      </Routes>
+    </AnimatePresence>
     // </UserProvider>
   );
 }
