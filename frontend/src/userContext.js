d51b30adf5f4a8eb9fe889ce731db49394891b72
Lyla Liu <lylaliu@Lylas-MacBook-Pro.local>
2024-11-07 05:04:27 -0800

    authService and userContext

--- frontend/src/userContext.js
+++ frontend/src/userContext.js
@@ -1,19 +1,41 @@
-import React, { createContext, useContext, useState, useEffect } from "react";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
+// src/userContext.js
+import React, { createContext, useContext, useState } from "react";
+import { useNavigate } from 'react-router-dom';
+import { login as loginService, signUp as signUpService } from "./authService";
 
 const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
   const [uid, setUid] = useState(null);
+  const navigate = useNavigate();
+
+  const login = async (email, password) => {
+    try {
+      const userId = await loginService(email, password);
+      setUid(userId);
+      navigate("/home");
+    } catch (error) {
+      console.error("Login error:", error.message);
+      return error.message;
+    }
+  };
+
+  const signUp = async (email, password) => {
+    try {
+      const userId = await signUpService(email, password);
+      setUid(userId);
+      navigate("/home");
+    } catch (error) {
+      console.error("Signup error:", error.message);
+      return error.message;
+    }
+  };
 
   return (
-    <UserContext.Provider value={{ uid, setUid }}>
+    <UserContext.Provider value={{ uid, setUid, login, signUp }}>
       {children}
     </UserContext.Provider>
   );
 };
 
-export const useUser = () => {
-  return useContext(UserContext);
-};
-
+export const useUser = () => useContext(UserContext);
