c5d642fd465c58c39b548e1e05c2934c4b76c001
Zifan Zhou <zifanzhou1024@gmail.com>
2024-11-16 01:22:26 -0800

    Updated frontend to fit Google Cloud Platform

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,9 +1,24 @@
+// List of browser-compatible fallbacks for Node.js core modules
+const fallbacks = {
+    os: require.resolve("os-browserify/browser"),
+    path: require.resolve("path-browserify"),
+    crypto: require.resolve("crypto-browserify"),
+    stream: require.resolve("stream-browserify"),
+    buffer: require.resolve("buffer/"),
+    assert: require.resolve("assert/"),
+    util: require.resolve("util/"),
+    http: require.resolve("stream-http"),
+    https: require.resolve("https-browserify"),
+    zlib: require.resolve("browserify-zlib"),
+    fs: false, // Explicitly mark 'fs' as unsupported in the browser
+};
+
 module.exports = {
     webpack: {
         configure: (webpackConfig) => {
             webpackConfig.resolve.fallback = {
                 ...webpackConfig.resolve.fallback,
-                os: require.resolve("os-browserify/browser"),
+                ...fallbacks,
             };
             return webpackConfig;
         },
